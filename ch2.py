1d7c0d45a411c23dba878a894bd8ac0d481a7e01
Yas Okada <yas.okadatech@gmail.com>
2019-09-08 17:28:40 +0900

    2.1.1

---
+++ ch2.py
@@ -0,0 +1,25 @@
+from matplotlib import pyplot as plt
+from sklearn.datasets import load_iris
+import numpy as np
+
+data = load_iris()
+# print(data)
+
+features = data['data']
+#print(features)
+feature_names = data['feature_names']
+#print(feature_names)
+target=data['target']
+#print(target)
+target_names=data['target_names']
+#print(target_names)
+labels=target_names[target]
+#print(labels)
+
+for t,marker,c in zip(range(3),'>ox','rgb'):
+  plt.scatter(features[target == t,0],
+              features[target == t,1],
+              marker=marker,
+              c=c)
+
+plt.show()
