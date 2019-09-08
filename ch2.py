fc383dbd91daabca0edc2cc804ab029d525d1a68
Yas Okada <yas.okadatech@gmail.com>
2019-09-08 19:22:51 +0900

    apply_model

--- ch2.py
+++ ch2.py
@@ -16,6 +16,25 @@ target_names=data['target_names']
 labels=target_names[target]
 #print(labels)
 
+# Petal length
+plength=features[:,2]
+is_setosa = (labels == 'setosa')
+
+max_setosa=plength[is_setosa].max()
+min_non_setosa=plength[~is_setosa].min()
+print('Maximum of setosa: {0}.'.format(max_setosa))
+print('Minimum of others: {0}.'.format(min_non_setosa))
+
+def apply_model( example ):
+  if example[2] < 2: print('Iris Setosa')
+  else: print('Iris Virginica or Iris Versicolor')
+
+features = features[~is_setosa]
+labels = labels[~is_setosa]
+virginica = (labels == 'virginica')
+
+print(virginica)
+
 for t,marker,c in zip(range(3),'>ox','rgb'):
   plt.scatter(features[target == t,0],
               features[target == t,1],
