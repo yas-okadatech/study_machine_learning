b73800821bf18ebe03a3247f4d9769b8a0150cd4
Yas Okada <yas.okadatech@gmail.com>
2019-09-08 20:22:25 +0900

    apply_model2

--- ch2_1_2.py
+++ ch2_1_2.py
@@ -25,7 +25,7 @@ min_non_setosa=plength[~is_setosa].min()
 print('Maximum of setosa: {0}.'.format(max_setosa))
 print('Minimum of others: {0}.'.format(min_non_setosa))
 
-def apply_model( example ):
+def apply_model1( example ):
   if example[2] < 2: print('Iris Setosa')
   else: print('Iris Virginica or Iris Versicolor')
 
@@ -33,12 +33,32 @@ features = features[~is_setosa]
 labels = labels[~is_setosa]
 virginica = (labels == 'virginica')
 
-print(virginica)
+# print(labels)
+# print(virginica)
 
-for t,marker,c in zip(range(3),'>ox','rgb'):
-  plt.scatter(features[target == t,0],
-              features[target == t,1],
-              marker=marker,
-              c=c)
+best_acc = -1.0
+best_fi = -1.0
+best_t = -1.0
 
-plt.show()
+for fi in range(features.shape[1]):
+  thresh = features[:, fi].copy()
+  thresh.sort()
+
+  for t in thresh:
+    pred = (features[:,fi] > t)
+    acc = (labels[pred] == 'virginica').mean()
+    # print('acc: %f, fi: %f, t: %f' % (acc, fi, t))
+    if acc > best_acc:
+      best_acc = acc
+      best_fi = fi
+      best_t = t
+
+print('best_acc: %f' % best_acc)
+print('best_fi: %f' % best_fi)
+print('best_t: %f' % best_t)
+
+def apply_model2( example ):
+  if example[best_fi] > best_t: print('virginica')
+  else: print('versicolor')
+
+
