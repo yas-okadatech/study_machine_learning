3673b00b11d6ad31affca81a25c03a1d28f4bbc8
Yas Okada <yas.okadatech@gmail.com>
2019-09-10 07:18:02 +0900

    fit_transform and get_feature_names

--- ch3_2.py
+++ ch3_2.py
@@ -1,4 +1,8 @@
 from sklearn.feature_extraction.text import CountVectorizer
 vectorizer = CountVectorizer(min_df=1)
 
-print(vectorizer)
+# print(vectorizer)
+
+content = ['How to format my hard disk', ' Hard disk format problems ']
+x = vectorizer.fit_transform(content)
+print( vectorizer.get_feature_names() )
