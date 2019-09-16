cefe83c52de98b7e54e89998a630b20116993a48
Yas Okada <yas.okadatech@gmail.com>
2019-09-16 06:52:45 +0400

    dist_norm

--- ch3_2.py
+++ ch3_2.py
@@ -28,6 +28,12 @@ def dist_raw(v1, v2):
   delta = v1-v2
   return sp.linalg.norm(delta.toarray())
 
+def dist_norm(v1, v2):
+  v1_normalized = v1 / sp.linalg.norm(v1.toarray())
+  v2_normalized = v2 / sp.linalg.norm(v2.toarray())
+  delta = v1_normalized - v2_normalized
+  return sp.linalg.norm(delta.toarray())
+
 best_doc = None
 best_dist = sys.maxsize
 best_i = None
@@ -36,8 +42,13 @@ for i in range(0, num_samples):
   if post==new_post:
     continue
   post_vec = X_train.getrow(i)
-  d = dist_raw(post_vec, new_post_vec)
+  # d = dist_raw(post_vec, new_post_vec)
+  d = dist_norm(post_vec, new_post_vec)
   print('=== Post %i with dist=%.2f: %s' % (i, d, post))
+  if d<best_dist:
+    best_dist = d
+    best_i = i
+print('Best post is %i with dist=%.2f' % (best_i, best_dist))
 
 print(X_train.getrow(3).toarray())
 print(X_train.getrow(4).toarray())
